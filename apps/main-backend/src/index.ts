3aec0774d46dc971ab13bae4de7afb6d38443a54
dev0jha <ojha6773@gmail.com>
2026-02-20 01:12:50 +0530

    feat(auth): implement authentication module with signup and signin endpoints

--- apps/main-backend/src/index.ts
+++ apps/main-backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
+import {app as authApp} from "./modules/auth"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(authApp).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+ 
