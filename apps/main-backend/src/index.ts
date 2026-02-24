702c03b32b48ffcc478a0d0b7f84f043540d0a4f
dev0jha <ojha6773@gmail.com>
2026-02-25 02:20:45 +0530

    feat(payment): implement payment module with onramp functionality and JWT authentication

--- apps/main-backend/src/index.ts
+++ apps/main-backend/src/index.ts
@@ -1,9 +1,17 @@
 import { Elysia } from "elysia";
-import {app as authApp} from "./modules/auth"
 
-const app = new Elysia().use(authApp).listen(3000);
+import { app as apiKeysApp } from "./modules/apikeys";
+import { app as authApp } from "./modules/auth";
+import { app as modelsApp } from "./modules/models";
+import { app as paymentApp } from "./modules/payment";
+
+const app = new Elysia()
+  .use(authApp)
+  .use(modelsApp)
+  .use(apiKeysApp)
+  .use(paymentApp)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
- 
