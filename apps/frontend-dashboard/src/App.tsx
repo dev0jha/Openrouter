6ddb1279437d0fbb75cf7a192d55710e4cbc4eaf
dev0jha <ojha6773@gmail.com>
2026-02-25 03:01:39 +0530

    feat: update dependencies and implement routing in App component

--- apps/frontend-dashboard/src/App.tsx
+++ apps/frontend-dashboard/src/App.tsx
@@ -1,31 +1,42 @@
-import { APITester } from "./APITester";
-import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+import { treaty } from "@elysiajs/eden";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { App } from "app";
+
+import "./index.css";
+import { ApiKeys } from "./pages/ApiKeys";
+import { Credits } from "./pages/Credits";
+import { Dashboard } from "./pages/Dashboard";
+import { Landing } from "./pages/Landing";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { ElysiaClientContextProvider } from "./providers/Eden";
+
+const client = treaty<App>("localhost:3000", {
+  fetch: {
+    credentials: "include",
+  },
+});
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-        />
-      </div>
-
-      <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
-      <p>
-        Edit <code className="bg-[#1a1a1a] px-2 py-1 rounded font-mono">src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ElysiaClientContextProvider value={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<Landing />} />
+            <Route path={"/signup"} element={<Signup />} />
+            <Route path={"/signin"} element={<Signin />} />
+            <Route path={"/dashboard"} element={<Dashboard />} />
+            <Route path={"/credits"} element={<Credits />} />
+            <Route path={"/api-keys"} element={<ApiKeys />} />
+          </Routes>
+        </BrowserRouter>
+      </ElysiaClientContextProvider>
+    </QueryClientProvider>
   );
 }
 
